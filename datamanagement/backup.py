ecef183c241731fb87a1a2d7ca3c4cfa75cffab0
akh2103 <akh2103@gmail.com>
2015-02-13 17:35:55 -0600

    fixestobackupscript

--- datamanagement/backup.py
+++ datamanagement/backup.py
@@ -1,8 +1,44 @@
 import os, time, json
 from documentcloud import DocumentCloud
 
+import datetime as dt
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from vaultclasses import Contract
+import ConfigParser
+from sqlalchemy import desc
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from dateutil import parser
+import csv
+
+
+Base = declarative_base()
+
+CONFIG_LOCATION = '/apps/contracts/app.cfg'
+
+def get_from_config(field):
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_LOCATION)
+    return config.get('Section1', field)
+
+server = get_from_config('server')
+databasepassword = get_from_config('databasepassword')
+user = get_from_config('user')
+database = get_from_config('database')
+
+engine = create_engine('postgresql://abe:' + databasepassword + '@' + server + ':5432/' +  database)
+Session = sessionmaker(bind=engine)
+Session.configure(bind=engine)
+session = Session()
+
+def get_all_contract_ids():
+	dcids = [i[0] for i in session.query(Contract.doc_cloud_id).order_by(desc(Contract.dateadded)).all()]
+	return dcids
+
+doc_cloud_ids = get_all_contract_ids()
+
 client = DocumentCloud()
-docs = client.documents.search('projectid: 1542-city-of-new-orleans-contracts')
 
 BASEBACKUP = "/Volumes/usb/"
 LOGFILE = "/Volumes/usb/log.txt"
@@ -28,24 +64,39 @@ def getMetaData(doc):
 	return metadata
 
 
-def backup(doc):
-	pdf = doc.pdf
-	metadata = getMetaData(doc)
-	if not os.path.exists(BASEBACKUP + "/" +doc.id.replace("/","") + ".pdf"):
+def needs_to_be_backed_up(doc_cloud_id):
+	if not os.path.exists(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + ".pdf"):
+		return True
+
+	if not os.path.exists(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + ".txt"):
+		return True
+
+	if not os.path.exists(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + "_text" + ".txt"):
+		return True
+
+	return False
+
+def backup(doc_cloud_id):
+	if needs_to_be_backed_up(doc_cloud_id):
+		print "{} needs to be backed up".format(doc_cloud_id)
+		doc = client.documents.get(doc_cloud_id)
 		pdf = doc.pdf
-		with open(BASEBACKUP + "/" +doc.id.replace("/","") + ".pdf", "wb") as f:
-			f.write(pdf)
+		metadata = getMetaData(doc)
+		if not os.path.exists(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + ".pdf"):
+			pdf = doc.pdf
+			with open(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + ".pdf", "wb") as f:
+				f.write(pdf)
 
-	if not os.path.exists(BASEBACKUP + "/" +doc.id.replace("/","") + ".txt"):
-		with open(BASEBACKUP + "/" +doc.id.replace("/","") + ".txt", "wb") as f:
-			f.write(json.dumps(metadata))
+		if not os.path.exists(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + ".txt"):
+			with open(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + ".txt", "wb") as f:
+				f.write(json.dumps(metadata))
 
-	if not os.path.exists(BASEBACKUP + "/" +doc.id.replace("/","") + "_text" + ".txt"):
-		with open(BASEBACKUP + "/" +doc.id.replace("/","") + ".txt", "wb") as f:
-			f.write(json.dumps(metadata))
+		if not os.path.exists(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + "_text" + ".txt"):
+			with open(BASEBACKUP + "/" + doc_cloud_id.replace("/","") + "_text", "wb") as f:
+				f.write(json.dumps(doc.full_text))
 
-for doc in docs:
-	try:
-		backup(doc)
-	except:
-		pass
+for doc_cloud_id in doc_cloud_ids:
+	print "checking {}".format(doc_cloud_id)
+	backup(doc_cloud_id)
+
+session.close()
