43ae4a103765806f2296192dad584f30609cba8a
AbeHandler <akh2103@gmail.com>
2015-04-14 10:49:18 -0500

    adding tests

--- app/app.py
+++ app/app.py
@@ -52,17 +52,19 @@ else:
 @cache.memoize(timeout=900)
 def queryDocumentCloud(searchterm):
     """
-    This is it's own method so that queries can be cached via @memoize to speed things up
+    This is it's own method so that queries
+    can be cached via @memoize to speed things up
     """
     return documentCloudClient.documents.search(searchterm)
 
 
 def translateToDocCloudForm(docs):
     """
-    In the database each row for contracts has an ID which is different from the 
-    doc_cloud_id on document cloud. This just translates rows so that their 
-    id is equal to the doc_cloud_id. It's a bit awkward and should probably 
-    be refactored away at some point. 
+    In the database each row for contracts has an ID which
+    is different from the doc_cloud_id on document cloud.
+    This just translates rows so that their id is equal
+    to the doc_cloud_id. It's a bit awkward and should
+    probably be refactored away at some point.
     """
     for d in docs:
         d.id = d.doc_cloud_id
@@ -106,7 +108,9 @@ def getVendors():
     Session = sessionmaker(bind=engine)
     Session.configure(bind=engine)
     session = Session()
-    vendors = session.query(Vendor.name).filter(Vendor.id==Contract.vendorid).distinct().order_by(Vendor.name)
+    vendors = session.query(Vendor.name).\
+        filter(Vendor.id == Contract.vendorid).\
+        distinct().order_by(Vendor.name)
     vendors = sorted(list(set([j[0].strip() for j in vendors])))
     vendors.insert(0, "Vendor (example: Three fold Consultants)".upper())
     session.close()
@@ -126,7 +130,7 @@ def getDepartments():
     depts = [j[0].strip() for j in depts]
     depts = sorted(list(set(depts)))
     depts.insert(0, "Department (example: Information Technology)".upper())
-    session.close
+    session.close()
     return depts
 
 
@@ -141,7 +145,7 @@ def getOfficers(vendor=None):
     if vendor is None:
         officers = session.query(VendorOfficer, Person).\
             filter(VendorOfficer.personid == Person.id).order_by(Person.name)
-        session.close
+        session.close()
         officers = sorted(list(set([o[1].name for o in officers])))
         message = "Company officer (example: Joe Smith) - feature in progress"
         officers.insert(0, message.upper())
@@ -153,7 +157,7 @@ def getOfficers(vendor=None):
             filter(VendorOfficer.personid == Person.id).\
             filter(VendorOfficer.vendorid == Vendor.id).\
             filter(Vendor.name == vendor).all()
-        session.close
+        session.close()
         officers = list(set([o[1].name for o in officers]))
         print officers
         return sorted(officers)
@@ -206,7 +210,7 @@ def getPages(searchterm):
 def getTotalDocs(searchterm):
     """
     Get the total number of relevant docs for a given search.
-    """   
+    """
     if searchterm == 'projectid: "1542-city-of-new-orleans-contracts"':
         return getContracts_Count()
     else:
@@ -216,12 +220,17 @@ def getTotalDocs(searchterm):
 def getStatus(page, total, searchterm):
     """
     Tells the user what search has returned
-    """   
+    """
+    basic_query = 'projectid: "1542-city-of-new-orleans-contracts"'
     output = ""
-    output = output + str(total) + " results | Query: " + searchterm.replace('projectid:"1542-city-of-new-orleans-contracts"', "")
+    output = output + str(total) \
+        + " results | Query: " + \
+        searchterm.\
+        replace(basic_query, "")
     output = output + " | "
-    if searchterm == 'projectid: "1542-city-of-new-orleans-contracts"':
-        return output + "All city contracts: page " + str(page) + " of " + str(total)
+    if searchterm == basic_query:
+        return output + "All city contracts: page "\
+            + str(page) + " of " + str(total)
     else:
         return output + "Page " + str(page) + " of " + str(total)
 
@@ -231,14 +240,9 @@ def vendors(q=None):
     """
     Get requested vendors from a query string and return a template.
     Needs to be refactored using the flask query parser
-    """   
+    """
     if q == "all":
         vendors = getVendors()
-    else:
-        # to do: use flasks query parsers
-        q = q.split("=")[1]
-        terms = [t for t in q.split("&") if len(t) > 0]
-        vendor = [v.replace("vendor:", "") for v in terms if "vendor:" in v].pop()
     return render_template('select.html', options=vendors)
 
 
@@ -269,7 +273,6 @@ def intro(name=None):
     """
     Intro page for the web app
     """
-    offset = 0
     docs = getContracts(0, PAGELENGTH)
     totaldocs = getContracts_Count()
     pages = int(totaldocs/PAGELENGTH) + 1
@@ -285,7 +288,7 @@ def intro(name=None):
                            totaldocs=totaldocs,
                            pages=pages,
                            page=1,
-                           status = status,
+                           status=status,
                            docs=docs,
                            officers=officers,
                            query=dc_query,
@@ -294,14 +297,15 @@ def intro(name=None):
                            url="contracts")
 
 
-
 @app.route('/contracts/contract/<string:doc_cloud_id>', methods=['GET'])
 def contract(doc_cloud_id):
     """
     Request for a given contract
     """
     doc_cloud_id = re.sub(".html$", "", doc_cloud_id)
-    return render_template('contract_child.html', doc_cloud_id=doc_cloud_id, title="New Orleans city contracts")
+    return render_template('contract_child.html',
+                           doc_cloud_id=doc_cloud_id,
+                           title="New Orleans city contracts")
 
 
 def query_request(field):
@@ -322,7 +326,7 @@ def translate_web_query_to_dc_query():
 
     for t in terms:
         query_value = query_request(t)
-        if query_request(t) !="":
+        if query_request(t) != "":
             qb.add_term(t, query_value)
 
     officers = query_request('officer')
@@ -330,9 +334,10 @@ def translate_web_query_to_dc_query():
     if len(officers) > 0:
         officers = [officers]
         vendor = translateToVendor(officers[0])
-        qb.add_term("vendor", vendor) 
+        qb.add_term("vendor", vendor)
 
     return qb.get_query()
+
 
 def get_offset(offset):
     logging.info("offset | {}".format(offset))
@@ -341,8 +346,9 @@ def get_offset(offset):
     elif offset < 0:
         offset = 0
     else:
-        offset = int(offset) - 1 # a page is one more than an offset
+        offset = int(offset) - 1  # a page is one more than an offset
     return offset
+
 
 @app.route('/contracts/search', methods=['POST', 'GET'])
 def query_docs():
@@ -374,31 +380,31 @@ def query_docs():
     officers = getOfficers()
     departments = getDepartments()
     logging.info('Pages = {}'.format(pages))
-
+    standard_query = 'projectid: "1542-city-of-new-orleans-contracts" '
     if request.method == 'GET':
         return render_template('intro_child.html',
                                vendors=vendors,
                                departments=departments,
                                offset=offset,
                                totaldocs=totaldocs,
-                               status = status,
+                               status=status,
                                pages=pages,
                                page=offset + 1,
                                docs=docs,
                                officers=officers,
-                               query=searchterm.replace('projectid: "1542-city-of-new-orleans-contracts" ',""),
+                               query=searchterm.replace(standard_query, ""),
                                title="New Orleans city contracts",
                                updated=updateddate,
                                url="contracts")
     if request.method == 'POST':
-        return render_template('documentcloud.html', 
-                           status=status,
-                           docs=docs,
-                           pages=pages,
-                           page=offset + 1,
-                           vendor=vendor,
-                           totaldocs=totaldocs,
-                           query=searchterm.replace('projectid: "1542-city-of-new-orleans-contracts" ',""))
+        return render_template('documentcloud.html',
+                               status=status,
+                               docs=docs,
+                               pages=pages,
+                               page=offset + 1,
+                               vendor=vendor,
+                               totaldocs=totaldocs,
+                               query=searchterm.replace(standard_query, ""))
 
 if __name__ == '__main__':
     app.run()
