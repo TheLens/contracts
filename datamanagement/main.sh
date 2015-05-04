43ef98c6a33a0379c4866df7fc066f733ca6f846
AbeHandler <abram.handler@gmail.com>
2015-05-04 14:24:47 -0500

    adding main sh

--- datamanagement/main.sh
+++ datamanagement/main.sh
@@ -4,8 +4,8 @@ python contracts/datamanagement/main.py
 
 sleep 3h  #give document cloud 3 hours to process the new files. if by some chance it does not finish by then, the sync script will catch them the next time around
 
-python /apps/contracts/datamanagement/lensDocCloudSynch.py
+python /home/ubuntu/contracts/datamanagement/lensDocCloudSynch.py
 
-python /apps/contracts/datamanagement/daily_linker.py
-python /apps/contracts/datamanagement/emailer.py
-python /apps/contracts/datamanagement/backup.py
+python /home/ubuntu/contracts/datamanagement/daily_linker.py
+python /home/ubuntu/contracts/datamanagement/emailer.py
+python /home/ubuntu/contracts/datamanagement/backup.py
