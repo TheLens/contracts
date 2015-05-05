7877d3360b5202ea29c0b59b141fe3a8c622c396
AbeHandler <akh2103@gmail.com>
2015-05-04 23:03:06 -0500

    splitting ethics record into its own file

--- datamanagement/lib/models.py
+++ datamanagement/lib/models.py
@@ -179,8 +179,8 @@ class PurchaseOrder(object):
         '''
         Find the vendor id in the HTML
         '''
-        p = "(?<=ExternalVendorProfile\(')\d+"
-        vendorids = re.findall(p,html)
+        pattern = "(?<=ExternalVendorProfile\(')\d+"
+        vendorids = re.findall(pattern, html)
         if len(vendorids) == 0:
             return ""
         else:
@@ -252,64 +252,6 @@ class PurchaseOrder(object):
 
     def __str__(self):
         return "<PurchaseOrder {}>".format(self.purchaseorder)
-
-
-class EthicsRecord(Base):
-    """
-    This class simply represents a single row of 
-    campaign finance contributions (from the Louisiana Ethics Board). 
-
-    It goes in datamanagement instead of lib/models because it doesn't 
-    really concern the public web app
-    """
-    __tablename__ = 'ethics_records'
-
-    id = Column(Integer, primary_key=True)
-    last = Column(String)
-    first = Column(String)
-    reportno = Column(String)
-    form = Column(String)
-    schedule = Column(String)
-    contributiontype = Column(String)
-    contributorname = Column(String)
-    address1 = Column(String)
-    address2 = Column(String)
-    city = Column(String)
-    state = Column(String)
-    zipcode = Column(String)
-    receiptdate = Column(String)
-    receiptamount = Column(String)
-    description = Column(String)
-
-    def __init__(self, 
-                 last, first, reportno,
-                  form, schedule, contributiontype, 
-                  contributorname, address1, 
-                  address2, city, state, zipcode,
-                  receiptdate, receiptamount, description):
-        self.last = last
-        self.first = first
-        self.reportno = reportno
-        self.form = form
-        self.schedule = schedule
-        self.contributiontype = contributiontype
-        self.contributorname = contributorname
-        self.address1 = address1
-        self.address2 = address2
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-        self.receiptdate = receiptdate
-        self.receiptamount = receiptamount
-        self.description = description
-
-
-    def __str__(self):
-        return "${} to {} {} on {}".format(self.receiptamount, self.first, self.last, self.receiptdate)
-
-
-    def __repr__(self):
-        return "${} to {} {} on {}".format(self.receiptamount, self.first, self.last, self.receiptdate)
 
 
 class LensRepository(object):
@@ -512,7 +454,7 @@ class LensDatabase(object):
             department = Department(department)
             self.session.add(department)
             self.session.commit()
-        
+
 
     def add_contract(self, contract):
         """
@@ -581,7 +523,7 @@ class LensDatabase(object):
 
     def get_lens_vendor_id(self, vendor):
         """
-        Get a vendor in the db. To do: refactor 
+        Get a vendor in the db. To do: refactor
         """
         self.session.flush()
         vendor = self.session.query(Vendor).filter(Vendor.name==vendor).first()
