31d7a98dae6f9c39a9f6cf7263c6be0df6d34fd0
Thomas Thoren <thomasjthoren@gmail.com>
2015-05-15 20:08:18 -0500

    Fixed .coveragerc and added more files to it.

--- contracts/static/js/contract.js
+++
@@ -1,5 +0,0 @@
-$(".download").on("click", function() {
-  // var id = $(this).parents(".contract-row").attr("id");
-  var id = document.querySelector('#download').getAttribute('data-id');
-  downloadFile("/contracts/download/" + id);
-});
