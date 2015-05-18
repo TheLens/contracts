3e33c863321ef11c8581d4df4d1e9041fd8cbcd5
Thomas Thoren <thomasjthoren@gmail.com>
2015-05-18 00:38:43 -0500

    Added clear-all function in advanced search area.

--- contracts/static/js/search.js
+++ contracts/static/js/search.js
@@ -17,10 +17,11 @@ function prepareData() {
   } else {
     // console.log(data.current_page);
     // console.log(typeof data.current_page);
-    data.current_page = document.querySelector('#pagination').getAttribute('data-current-page');
+    var current_page = document.querySelector('#pagination').getAttribute('data-current-page');
+    data.current_page = parseInt(current_page, 10);
   }
 
-  console.log(data);
+  console.log('Parameters collected:', data);
 
   return data;
 }
@@ -53,7 +54,7 @@ function buildSearch(data) {
     query_string = query_string + "officer=" + data.officer;
   }
 
-  if (data.current_page !== '' && data.current_page !== "1") {
+  if (data.current_page !== '' && data.current_page !== 1) {
     if (query_string !== '?') {
       query_string = query_string + '&';
     }
@@ -100,23 +101,85 @@ function setHandlers() {
       document.getElementById('advanced-search').innerHTML = 'Hide advanced search <i class="fa fa-caret-up"></i>';
     }
   });
+
+  $(".clear-all").on("click", function() {
+    document.getElementById('text-box').value = '';
+    document.getElementById('vendors').value = '';
+    document.getElementById('departments').value = '';
+    document.getElementById('officers').value = '';
+  });
 }
 
 function populateSearchParameters(data) {
-  console.log(data);
-  //debugger;
 
-  // document.getElementById('text-box').value = data.search_input;
-  // document.getElementById('vendors').value = data.vendors;
-  // document.getElementById('departments').value = data.departments;
-  // document.getElementById('officers').value = data.officers;
+  console.log('Parameter data returned:', data);
+
+  document.getElementById('text-box').value = data.search_input;
+  document.getElementById('vendors').value = data.vendor;
+  document.getElementById('departments').value = data.department;
+  document.getElementById('officers').value = data.officer;
+
+  if (data.vendor !== '' || data.department !== '' || data.officer !== '') {
+    document.getElementById('filters').style.display = 'block';
+    document.getElementById('advanced-search').innerHTML = '<a>Hide advanced search <i class="fa fa-caret-up"></i></a>';
+  }
 }
 
-function getSearch() {
+// function getDataFromUrl() {
+//   var data = {};
+
+//   // data.search_input = encodeURIComponent($("#text-box").val());
+//   // data.vendor = encodeURIComponent($('#vendors').val());
+//   // data.department = encodeURIComponent($('#departments').val());
+//   // data.officer = encodeURIComponent($('#officers').val());
+//   // data.current_page = document.querySelector('#pagination');
+
+
+//   var url_query = window.location.search.substring(0).match(/query\=(.*)/i);
+//   if (url_query !== null) {
+//     var query_text = decodeURIComponent(url_query[1]);
+//     data.search_input = query_text;
+//   }
+
+//   var url_vendors = window.location.search.substring(0).match(/vendors\=(.*)/i);
+//   if (url_vendors !== null) {
+//     var vendor_text = decodeURIComponent(url_vendors[1]);
+//     data.vendor = vendor_text;
+//   }
+
+//   var url_departments = window.location.search.substring(0).match(/departments\=(.*)/i);
+//   if (url_departments !== null) {
+//     var departments_text = decodeURIComponent(url_departments[1]);
+//     data.department = departments_text;
+//   }
+
+//   var url_officers = window.location.search.substring(0).match(/officers\=(.*)/i);
+//   if (url_officers !== null) {
+//     var officers_text = decodeURIComponent(url_officers[1]);
+//     data.officer = officers_text;
+//   }
+
+//   var url_page = window.location.search.substring(0).match(/page\=(.*)/i);
+//   if (url_page !== null) {
+//     var page_text = decodeURIComponent(url_page[1]);
+//     data.current_page = page_text;
+//   }
+
+//   return data;
+// }
+
+function getSearch(keep_current_page) {
   var data = prepareData();
 
+  if (keep_current_page !== 1) {
+    data.current_page = 1;
+  }
+
+  // data = checkForNewSearchParameters(data);
+
   var query_string = buildSearch(data);
-  console.log(query_string);
+  
+  console.log('Query string:', query_string);
   
   window.location.href = '/contracts/search/' + query_string;
 }
